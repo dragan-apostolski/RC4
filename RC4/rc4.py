eff17deff0751a55a14a5acfa667588d8b9d6714
Dragan Apostolski <drapostolski@gmail.com>
2017-11-25 17:13:29 +0100

    Fix bugs in the PRGA implementation

--- RC4/rc4.py
+++ RC4/rc4.py
@@ -6,7 +6,7 @@
 За генерирање на клуч од b бајти и иницијален клуч k, се повикува функцијата generate_key_stream(k, b). k мора да биде
 во bytes објект. Пример:
 
-    init_key = b'0102030405'
+    key = b'0102030405'
     key_len = 40
     key = generate_key_stream(init_key, key_len)
 
@@ -18,7 +18,7 @@
 
 Шифрираниот текст во cipher_text е исто така во форма на bytes објект. Истиот текст, со истиот клуч може да се дешифрира
 со повик до функцијата decrypt на следниов начин:
-
+57 49 41 33 25 17 9 1
     original_text = decrypt(cipher_text, key)
 """
 
@@ -56,48 +56,59 @@ class StateVector:
         return self.s[item]
 
 
-def generate_key_stream(initial_key, length):
+def print_ks_arr(ks_arr):
+    for c in ks_arr:
+        print('{:02x} '.format(c), end="")
+    print()
+
+
+def generate_key_stream(initial_key, offset=0, length=16):
     """Оваа функција генерира клуч со должина length, врз основа на иницијалниот клуч initial_key. Прво се иницијализира
     објект од класата StateVector, кој што ќе биде енкапсулација на векторот на состојба s. Вредностите (бајтите) за
     клучот се случајно избрани вредности од векторот на состојба, а по секоја избрана вредност за клучот се врши нова
     пермутација на s со повик на функцијата swap
 
-    :param initial_key bytes објект кој ги содржи бајтите на иницијалниот клуч
-    :param length должина на клучот кој ќе биде вратен
+    :param initial_key: bytes објект кој ги содржи бајтите на иницијалниот клуч
+    :param offset: после која позиција да почнат да се користат бајтите на клучот
+    :param length: должина на клучот
     :return низа со должина length, која ги содржи вредностите на клучот во форма на броеви од опсегот [0 - 255]
     """
     state_vector = StateVector(initial_key)
-    key = []
     i = j = 0
-    for k in range(length):
+    count = 0
+    while count < length + offset:
         i = (i + 1) % 256
         j = (j + state_vector[i]) % 256
         state_vector.swap(i, j)
-        key.append(state_vector[(state_vector[i] + state_vector[j]) % 256])
-    return key
+        if count >= offset:
+            yield (state_vector[(state_vector[i] + state_vector[j]) % 256])
+        count += 1
 
 
-def encrypt(plain_text, key):
+def encrypt(plain_text, key_stream, offset):
     """Примитивата encrypt служи за енкриптирање на текстот даден во plain_text со клуч key.
      Клучот треба да биде генериран со помош на функцијата generate_key_stream.
 
      :param plain_text текстот (во bytes објект) што треба да биде шифриран
-     :param key клучот кој ќе се употреби во шифрирањето
+     :param key_stream клучот кој ќе се употреби во шифрирањето
+     :param offset од која позиција на key stream-от да почнат да се користат бајтите
      :return bytes објект кој го содржи шифрираниот текст
      """
-    key_length = len(key)
-    ciphered_text = bytearray(plain_text)
-    for i in range(len(plain_text)):
-        enc = ((plain_text[i] + key[i % key_length]) % 256)
-        ciphered_text[i] = enc
-    return bytes(ciphered_text)
+    for i in range(offset):
+        key_stream.__next__()
+    cipher_text = ""
+    for c in plain_text:
+        letter = ("%02X" % (c ^ key_stream.__next__()))
+        cipher_text += letter
+    return cipher_text
 
 
-def decrypt(ciphered_text, key):
+def decrypt(ciphered_text, key, offset):
     """Примитивата decrypt служи за дешифрирање на шифрираниот текст ciphered_text со клуч key.
 
     :param ciphered_text шифрираниот текст во форма на bytes објект
     :param key клучот за дешифрирање
+    :param offset
     :return дешифриран текст во форма на bytes објект
     """
     original = bytearray(ciphered_text)
